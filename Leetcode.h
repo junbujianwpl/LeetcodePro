860f564e1ffc490dde76873eecbcff6946fab960
allen <476461917@qq.com>
2017-09-02 22:01:55 +0800

    change signature time to times

--- Leetcode.h
+++ Leetcode.h
@@ -10,11 +10,37 @@ class Leetcode
 public:
     Leetcode();
     
-    static std::vector<std::vector<int> > twoSum(std::vector<int> arr,int dst, int *time=NULL);
+    /**
+     * @brief twoSum return all pairs whose sum is dst
+     * @param arr    input array
+     * @param dst    dst val
+     * @param times  run times
+     * @return       vector of all pairs
+     */
+    static std::vector<std::vector<int> > twoSum(std::vector<int> arr, int dst, int *times=NULL);
 
-    static std::vector<std::vector<int> > twoSumIndex(std::vector<int> arr,int dst, int *time=NULL);
+    /**
+     * @brief twoSumIndex     return all index pairs of two element whose sumary is dst
+     * @param arr	      input array
+     * @param dst	      dst val
+     * @param times	      run times
+     * @return 		      vector of pair's index
+     */
+    static std::vector<std::vector<int> > twoSumIndex(std::vector<int> arr, int dst, int *times=NULL);
 
-    static std::vector<std::vector<int> > threeSum(std::vector<int> arr,int dst, int *time=NULL);
+    /**
+     * @brief threeSum        return all 3d pairs whose sum is dst
+     * @param arr	      input array
+     * @param dst	      dst val
+     * @param times	      run times
+     * @return 		      vector of pair's index
+     */
+    static std::vector<std::vector<int> > threeSum(std::vector<int> arr, int dst, int *times=NULL);
+
+
+
+
+
 };
 
 #endif // LEETCODE_H
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # LeetcodePro
 my leetcode solutions in cpp 
 
+
+## Done
+        two sum
+        three sum
+        two sum index
+
+
